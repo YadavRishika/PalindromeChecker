59f92c7608890bbf4115136a8038417dedfa5993
YadavRishika <yadavrishika03@gmail.com>
2026-02-17 15:15:27 +0530

    UC2: Hardcoded palindrome

---
+++ src/UseCase1PalindromeCheckerApp.java
@@ -0,0 +1,14 @@
+public class UseCase1PalindromeCheckerApp {
+
+    static final String VERSION = "1.0.0";
+
+    public static void main(String[] args) {
+
+        System.out.println("WELCOME TO PALINDROME CHECKER APP");
+        //System.out.println("Application Name : Palindrome Checker");
+        System.out.println("Application Version : " + VERSION);
+
+        System.out.println("Application initialized Successfully.");
+    }
+}
+
